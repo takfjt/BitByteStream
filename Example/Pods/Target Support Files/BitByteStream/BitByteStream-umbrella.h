8dd08bbdcd971483759054c65509d96432e784cb
Takahiro Fujita <takfjt@gmail.com>
2015-08-11 05:33:55 +0900

    fix test pods

--- Example/Pods/Target Support Files/BitByteStream/BitByteStream-umbrella.h
+++ Example/Pods/Target Support Files/BitByteStream/BitByteStream-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "BitByteStream.h"
 
 FOUNDATION_EXPORT double BitByteStreamVersionNumber;
 FOUNDATION_EXPORT const unsigned char BitByteStreamVersionString[];
--- Example/Tests/BitByteStreamTest.m
+++ Example/Tests/BitByteStreamTest.m
@@ -21,7 +21,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
-#import "BitByteStream.h"
+#import <BitByteStream/BitByteStream.h>
 
 @interface BitByteStreamTest : XCTestCase
 
